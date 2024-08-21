071b126c718da5146bbf886117645def849a5f3e
Skull33 <alonzo.cuela@ucsp.edu.pe>
2024-08-20 23:51:00 -0500

    trabajos v0.01

---
+++ convertidor de numeros/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void convertir()
+{
+	string lista = [];
+}
+
+int main()
+{
+	unsigned int x = 0;
+	cout << "Ingrese un velor entero no negativo:" << endl;
+	cin >> x;
+	cout << "el valor de x es: " << x << endl;
+}
