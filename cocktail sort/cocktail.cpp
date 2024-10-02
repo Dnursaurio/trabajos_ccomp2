4b96b07010da2338f63121405d2be8e56515f664
Skull33 <alonzo.cuela@ucsp.edu.pe>
2024-10-02 17:49:22 -0500

    trabajos v0.15

--- cocktail sort/cocktail.cpp
+++ cocktail sort/cocktail.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void print(int*p, int A[9])
+void print(int n, int A[])
 {
-	for (p = A; p < A+9; p++)
+	for (int p = 0; p < n; p++)
 	{
-		cout << *p<<" ";
+		printf("%d ", A[p]);
 	}
 	cout << endl;
 }
 
 void cocktail(int *&ini, int *&fin)
 {
-	for (int *ptrrec = ini+1; ptrrec < fin;ptrrec++)
+	/*segun geeksforgeeks usa 3 
+	parametros uno que esta al incio del arreglo (ini),
+	uno al final (fin) y uno delante del parametro inicial*/
+	//usamos este parametro para dos tipos de recorrido uno hacia la derecha y otro a la izquierda
+	//el bucle sera mientras la posicion de ini sea < a la de fin
+	while (ini < fin)
 	{
-		if (*ini > *ptrrec)
+		//hacemos el recorrido hacia la derecha
+		for (int *extra = ini + 1; extra <= fin; extra++) //aseguramos el recorrido
 		{
-			int temp = *ini;
-			*ini = *ptrrec;
-			*ptrrec = temp;
+			if (*ini > *extra)
+			{
+				swap(*ini,*extra);
+			}
 		}
-		ptrrec++;
+		//retrocedemos el valor del final
 		ini++;
 	}
 }
@@ -31,11 +38,11 @@ int main()
 	int *p = C;
 	int *q = C + 8;
 	cout << "el array original es: ";
-	print(p, C);
+	print(9, C);
 	cout << "el valor del puntero inicial es: "<< *p << " el valor del puntero final es: "<< *q << endl;
 	cout << "el array con Cocktail es: ";
 	cocktail(p,q);
-	print(p,C);
+	print(9,C);
 	cout << "el valor del puntero inicial con Cocktail es: " << *p << " el valor del puntero final con Cocktail es: " << *q << endl;
 	return 0;
 }
